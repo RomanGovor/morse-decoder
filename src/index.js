0b041ad311b30ce8bcd848e2b5aaa2120dd0c3ae
Roman Govor 850503 <roma-govor2013@ya.ru>
2020-10-02 01:33:34 +0300

    Initial commmit

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '', buf = '';
+    for(let i = 0; i < expr.length; i += 10) {
+        let str = expr.substr(i,10);
+        for(let j = 0; j < str.length; j += 2) {
+            if(str[j] === '*') {buf = ' '; break;}
+            if(str[j] === '0') continue;
+            else if(str[j + 1] === '0') buf += ('.');
+            else buf += ('-');
+        }
+        if(buf !== ' ') result += MORSE_TABLE[buf];
+        else result += ' ';
+        buf = '';
+    }
+    return result;
 }
 
 module.exports = {
